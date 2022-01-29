f448c5d2d9c163091245b36067a93b2b88fb12fe
matr1x244 <zio-21@yandex.ru>
2022-01-29 18:58:31 +0300

    -

--- app/src/main/java/com/geekbrains/december/ui/films/main/FilmsViewModel.kt
+++ app/src/main/java/com/geekbrains/december/ui/films/main/FilmsViewModel.kt
@@ -2,8 +2,11 @@ package com.geekbrains.december.ui.films.main
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.geekbrains.december.model.AppState
 import com.geekbrains.december.model.repository.Repository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import java.lang.Thread.sleep
 
 
@@ -18,12 +21,11 @@ class FilmsViewModel(private val repository: Repository) : ViewModel() {
     private fun getDataFromLoad() {
 
         liveData.value = AppState.Loading
-            Thread {
-                sleep(1000)
-                liveData.postValue(AppState.Success(repository.getMovieFromServerTrends()))
-            }.start()
-
+        /*корутины без использования Thread*/
+        viewModelScope.launch(Dispatchers.IO) {
+            liveData.postValue(AppState.Success(repository.getMovieFromServerTrends()))
         }
+    }
 
     override fun onCleared() {
         super.onCleared()
